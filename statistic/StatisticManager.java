1ee269ee29965e93392659a69274e42901f9a5b3
Alex <46812651+qwedo@users.noreply.github.com>
2020-03-03 11:54:15 +0300

    Update StatisticManager.java

--- statistic/StatisticManager.java
+++ statistic/StatisticManager.java
@@ -12,13 +12,8 @@ import java.util.*;
 public class StatisticManager {
     private static StatisticManager instance;
     private StatisticStorage statisticStorage = new StatisticStorage();
-    //private Set<Cook> cooks = new HashSet<>();
 
     private StatisticManager(){}
-
-    /*public Set<Cook> getCooks() {
-        return cooks;
-    }*/
 
     public static StatisticManager getInstance(){
         if (instance == null){
@@ -33,10 +28,6 @@ public class StatisticManager {
         statisticStorage.put(data);
     }
 
-    /*public void register(Cook cook){
-        cooks.add(cook);
-    }*/
-
     public Map<LocalDate, Map<String, Integer>> getCookWorkLoadingData(){
         Map<LocalDate, Map<String, Integer>> cookMap = new TreeMap<>(Collections.reverseOrder());
         List<CookedOrderEventDataRow> eventList = castEventListToCookList(statisticStorage.getStorage(EventType.COOKED_ORDER));
@@ -46,7 +37,7 @@ public class StatisticManager {
     }
 
     public Map<LocalDate, Double> getAdData(){//достает все данные из хранилища, относящиеся к отображению рекламы, и считает общую прибыль за каждый день.
-        Map<LocalDate, Double> resultMap = new TreeMap<>(Collections.reverseOrder());//в конструктор передали компаратор, т.к. нужно в убывающем порядке
+        Map<LocalDate, Double> resultMap = new TreeMap<>(Collections.reverseOrder());
         List<VideoSelectedEventDataRow> eventList = castEventListToAdList(statisticStorage.getStorage(EventType.SELECTED_VIDEOS));//т.е. тип листа изначально EventDataRow
         for (VideoSelectedEventDataRow event : eventList)
             updateAdMap(resultMap, event);
@@ -56,7 +47,7 @@ public class StatisticManager {
     private void updateCookMap(Map<LocalDate, Map<String, Integer>> resultMap, CookedOrderEventDataRow event){
         LocalDate cookDate = convertToLocalDate(event.getDate());
         String cookName = event.getCookName();
-        Integer cookTime = event.getTime() % 60 == 0 ? event.getTime() / 60 : event.getTime() / 60 + 1;//округляем в большую сторону(по условию) Хотя по идее это не должно получиться
+        Integer cookTime = event.getTime() % 60 == 0 ? event.getTime() / 60 : event.getTime() / 60 + 1;
         if (!resultMap.containsKey(cookDate)){//если нет данных для этого дня
             Map<String, Integer> map = new TreeMap<>();
             map.put(cookName, cookTime);
@@ -92,7 +83,7 @@ public class StatisticManager {
 
     private List<VideoSelectedEventDataRow> castEventListToAdList(List<EventDataRow> list){
         List<VideoSelectedEventDataRow> eventList = new ArrayList<>();
-        for (EventDataRow originEvent : list)//прошлись по каждому элементу, добавили в новый лист и закастили его
+        for (EventDataRow originEvent : list)
             eventList.add((VideoSelectedEventDataRow) originEvent);
         return eventList;
     }
@@ -113,7 +104,7 @@ public class StatisticManager {
                 storage.put(eventType, new ArrayList<>());
         }
 
-        private void put(EventDataRow data){//модификатор private делает так, что к методу нельзя получить доступ за пределами класса StatisticManager. Особенности вложенных классов.
+        private void put(EventDataRow data){//модификатор private делает так, что к методу нельзя получить доступ за пределами класса StatisticManager.
             storage.get(data.getType()).add(data);//нашли в мапе тип события и в ArrayList добавили это событие
         }
 
