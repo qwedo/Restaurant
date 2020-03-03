8b8ba75d0796d2d0e15163066830c95adf829ee2
Alex <46812651+qwedo@users.noreply.github.com>
2020-03-03 11:56:12 +0300

    Update TestOrder.java

--- kitchen/TestOrder.java
+++ kitchen/TestOrder.java
@@ -14,11 +14,6 @@ public class TestOrder extends Order {
     //инициализация списка блюд для заказа рандомными значениями
     @Override
     protected void initDishes(){
-/*        dishes = new ArrayList<>(Arrays.asList(Dish.values()))
-                .stream()
-                .filter((d) -> new Random().nextDouble() > 0.5)
-                .collect(Collectors.toList());*/
-
         for (int i = 0; i < Dish.values().length; i++) {
             dishes.add(Dish.values()[new Random().nextInt(Dish.values().length - 1) + 1]);
         }
