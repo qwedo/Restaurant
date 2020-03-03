9e5eaa6a4aa7309656237412233456fe6ed25e3f
Alex <46812651+qwedo@users.noreply.github.com>
2020-03-03 12:00:46 +0300

    Update AdvertisementManager.java

--- ad/AdvertisementManager.java
+++ ad/AdvertisementManager.java
@@ -16,14 +16,11 @@ public class AdvertisementManager {
     private int timeSeconds;//время выполнения заказа поваром(в которое должны уложиться все наши ролики)
     private long bestPrice = 0;
 
-
     public AdvertisementManager(int timeSeconds) {
         this.timeSeconds = timeSeconds;
     }
 
-
-
-/*    public void processVideos(){
+    public void processVideos(){
         if (storage.list().size() == 0){ //Если нет рекламных видео, которые можно показать посетителю
             throw new NoVideoAvailableException();
         }
@@ -37,9 +34,12 @@ public class AdvertisementManager {
         }
     }
 
+    //сортировка в порядке уменьшения стоимости показа одного рекламного ролика в копейках
+    //после сортировка по увеличению стоимости показа одной секунды рекламного ролика в тысячных частях копейки
     private void sortAds(List<Advertisement> ads){
-        ads.sort(Comparator.comparingLong(Advertisement::getAmountPerOneDisplaying).reversed().thenComparing(ad -> ad.getAmountPerOneDisplaying() * 1000 / ad.getDuration()));//сортировка в порядке уменьшения стоимости показа одного рекламного ролика в копейках
-        //после сортировка по увеличению стоимости показа одной секунды рекламного ролика в тысячных частях копейки
+        ads.sort(Comparator.comparingLong(Advertisement::getAmountPerOneDisplaying)
+                 .reversed()
+                 .thenComparing(ad -> ad.getAmountPerOneDisplaying() * 1000 / ad.getDuration()));
     }
 
     private void showAds(List<Advertisement> ads){//Отобразить все рекламные ролики. для каждого показа сделать ревалидейт!
@@ -49,7 +49,8 @@ public class AdvertisementManager {
         }
     }
 
-    private List<Advertisement> getAdsWithHits (List<Advertisement> ads){ //выбираем только те рекламные ролики, в которых количество показов(hits) > 0. Чтобы не создавать дополнительный геттер, сделал это через getAmountPerOneDisplaying
+    //выбираем только те рекламные ролики, в которых количество показов(hits) > 0. Чтобы не создавать дополнительный геттер, сделал это через getAmountPerOneDisplaying
+    private List<Advertisement> getAdsWithHits (List<Advertisement> ads){
         return ads.stream().filter((ad) -> ad.getAmountPerOneDisplaying() > 0).collect(Collectors.toList());
     }
 
@@ -89,129 +90,5 @@ public class AdvertisementManager {
 
     private int CalcTime (List<Advertisement> ads){
         return ads.stream().mapToInt(Advertisement::getDuration).sum();
-    }*/
-
-
-    //скопировал метод, т.к. мой не проходил в 16 задаче
-    public void processVideos() throws NoVideoAvailableException {
-
-        List<Advertisement> videos = storage.list();
-        if (storage.list().isEmpty())
-            throw new NoVideoAvailableException();
-
-        // ищем список видео для показа согласно критериям
-        List<Advertisement> bestAds = new VideoHelper().findAllYouNeed();
-
-        // сортируем полученный список
-        Collections.sort(bestAds, new Comparator<Advertisement>() {
-            @Override
-            public int compare(Advertisement video1, Advertisement video2) {
-                long dif = video2.getAmountPerOneDisplaying() - video1.getAmountPerOneDisplaying();
-                if (dif == 0) dif = video2.getDuration() - video1.getDuration();
-                return (int) dif;
-            }
-        });
-        long amount = 0;
-        int totalDuration = 0;
-        for (Advertisement ad : bestAds) {
-            totalDuration += ad.getDuration();
-            amount += ad.getAmountPerOneDisplaying();
-        }
-        StatisticManager.getInstance().register(new VideoSelectedEventDataRow(bestAds, amount, totalDuration));
-        //StatisticManager.getInstance().register(new VideoSelectedEventDataRow(videosToShow, totalAmount, totalDuration));
-
-        // выводим список
-        for (Advertisement ad : bestAds) {
-            ConsoleHelper.writeMessage(ad.getName() + " is displaying... " +
-                    ad.getAmountPerOneDisplaying() + ", " +
-                    1000 * ad.getAmountPerOneDisplaying() / ad.getDuration());
-            ad.revalidate();
-        }
-    }
-
-    private class VideoHelper {
-        private int bestPrice = 0;
-        private int maxTime = 0;
-        private int numberOfClips = 0;
-        private List<Advertisement> bestAds = new ArrayList<>();
-        private List<Advertisement> candidates = new ArrayList<>();
-
-        public List<Advertisement> findAllYouNeed() {
-            // отбор кандидатов
-            for (Advertisement ad : storage.list()) {
-                if (ad.getDuration() <= timeSeconds && ad.getHits() > 0)
-                    candidates.add(ad);
-            }
-            if (candidates.isEmpty()) {
-                throw new NoVideoAvailableException();
-            } else findBestAds(new BinaryPattern(candidates.size()));
-            return bestAds;
-        }
-
-        // рекурсивная функция формирования списка для показа
-        public void findBestAds(BinaryPattern pattern) {
-            while (true) {
-                checkAds(pattern.getPattern());
-                if (!pattern.full()) pattern.increment();
-                else break;
-                findBestAds(pattern);
-            }
-        }
-
-        // проверка очередного набора видеоклипов
-        private void checkAds(int[] pattern) {
-            int price = 0;
-            int time = 0;
-            List<Advertisement> list = new ArrayList<>();
-            for (int i = 0; i < candidates.size(); i++) {
-                price += candidates.get(i).getAmountPerOneDisplaying() * pattern[i];
-                time += candidates.get(i).getDuration() * pattern[i];
-                if (pattern[i] == 1) list.add(candidates.get(i));
-            }
-            if (time > timeSeconds) return;
-            if (!(price > bestPrice)) {
-                if (!(price == bestPrice && time > maxTime)) {
-                    if (!(price == bestPrice && time == maxTime && list.size() < numberOfClips)) {
-                        return;
-                    }
-                }
-            }
-            bestAds = list;
-            bestPrice = price;
-            maxTime = time;
-            numberOfClips = list.size();
-        }
-
-        // формирование двоичных масок для сбора списка видеоклипов
-        private class BinaryPattern {
-            private int length;
-            private int count;
-
-            public BinaryPattern(int size) {
-                this.length = size;
-                this.count = 0;
-            }
-
-            public int[] getPattern() {
-                String regString = Integer.toBinaryString(count);
-                int dif = length - regString.length();
-                int[] pattern = new int[length];
-                for (int j = dif; j < pattern.length; j++) {
-                    if (regString.charAt(j - dif) == '1') pattern[j] = 1;
-                    else pattern[j] = 0;
-                }
-                return pattern;
-            }
-
-            public void increment() {
-                count++;
-            }
-
-            ;
-
-            public boolean full() {
-                return count == (int) Math.pow(2, length) - 1;
-            }
-        }
     }
 }
